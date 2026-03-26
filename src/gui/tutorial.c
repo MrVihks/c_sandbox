e5c3fa7fee5050b2f19ff342366c9e4a9c2009be
MrVihks <viniciushkds@gmail.com>
2026-03-25 22:35:15 -0300

    finished tutorial gui

--- src/gui/tutorial.c
+++ src/gui/tutorial.c
@@ -2,6 +2,8 @@
 #include "raylib.h"
 #include "raygui.h"
 
+static bool panelClosed = false;
+
 void InitTutorial(){
     GuiSetStyle(DEFAULT, TEXT_SIZE, 25);
     GuiSetStyle(DEFAULT, BACKGROUND_COLOR, (int)ColorToInt((Color){200, 200, 200, 255}));
@@ -12,16 +14,25 @@ void DrawTutorial(){
     int screenHeight = GetScreenHeight();
 
     int panelWidth  = screenWidth - 100;
-    int panelHeight = 150;
+    int panelHeight = 200;
 
     int panelX = (screenWidth / 2) - (panelWidth / 2);
-    int panelY = 10;
-    
-    Rectangle tutorialPanel = {panelX, panelY, panelWidth, panelHeight};
-    GuiPanel(tutorialPanel, (const char*)0);
-    // DrawText("Sandbox feito em C", 10, 10,30, DARKGRAY);
-    // DrawText("Use Q para aumentar o zoom e E para diminuir", 10, 600 - 70,30, DARKGRAY);
-    // DrawText("Use as setinhas para mover a camera", 10, 600 - 40,30, DARKGRAY);
+    int panelY = 40;
+ 
+    int tutorialFontSize = 25;
+
+    if(!panelClosed){
+        Rectangle tutorialPanel = {panelX, panelY, panelWidth, panelHeight};
+        GuiPanel(tutorialPanel, (const char*)0);
+
+        DrawText("Tutorial Prime Sandbox", panelX + 10, panelY + 10, 40, DARKGRAY);      
+        DrawText("Use Q para aumentar o zoom e E para diminuir", panelX + 20, panelY + 70, tutorialFontSize, DARKGRAY);
+        DrawText("Use as setinhas para mover a camera", panelX + 20, panelY + 110, tutorialFontSize, DARKGRAY);
+
+        if(GuiButton((Rectangle){panelX + panelWidth - 160, panelY + panelHeight - 60, 150, 50}, "Fechar")){
+            panelClosed = true;
+        }
+    }
 }
 
 void UpdateTutorial(){
