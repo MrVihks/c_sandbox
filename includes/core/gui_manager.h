410436ddcb49e10403982f53116e4a47a74896ec
MrVihks <viniciushkds@gmail.com>
2026-03-25 22:47:21 -0300

    adding a gui manager

---
+++ includes/core/gui_manager.h
@@ -0,0 +1,8 @@
+#ifndef GUI_MANAGER_H
+#define GUI_MANAGER_H
+
+void GuiInit();
+void GuiDraw();
+void GuiUnload();
+
+#endif
---
+++ src/core/gui_manager.h
@@ -0,0 +1,7 @@
+#include "gui_manager.h"
+#include "raylib.h"
+#include "raygui.h"
+
+void GuiInit(){
+   // vou ajustar isso ainda
+}
